6f1690efe9a2a6030460cc4052f3cab8ccfa85cc
xutaotaotao <xutaotao1108@outlook.com>
2017-11-23 23:09:48 +0800

    returns empty string given empty string

---
+++ spec/main.test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by 徐涛焘 on 11/23/2017.
+ */
+"use strict"
+let main = require("../lib/main.js");
+let expect = require('chai').expect;
+describe("Word Frequency",function () {
+    it("returns empty string given empty string",function () {
+        let result = main('');
+        expect(result).to.equal('');
+    });
+});
